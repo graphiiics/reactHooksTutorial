6ba953ac06db4e5827dde19346c1477819b6abc2
Gael Manzanares <gaelmanzanares@MacBook-Air-de-Gael.local>
2022-04-22 19:27:10 -0500

    add material-ui

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Container, Typography } from '@material-ui/core';
 
 function index() {
   return (
-	<div>Home</div>
+	<Container>
+    <Typography variant='h3'>Home</Typography>
+  </Container>
   )
 }
 
